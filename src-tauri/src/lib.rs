589a25f020e5982b6ebd966972b58363bf0cb0d3
Test <ssatyamlal@gmail.com>
2026-05-14 16:50:48 +0530

    update the API link

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,10 +31,31 @@ struct DailyWeather {
     temperature_2m_min: Vec<f64>,
 }
 
+#[tauri::command]
+async fn fetch_weather() -> Result<WeatherResponse, String> {
+    let latitude = 28.6139;
+    let longitude = -77.2090;
+
+    let url = format!(
+        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FSingapore",
+        latitude,
+        longitude
+    );
+
+    let response = reqwest::get(&url).await.map_err(|e| e.to_string())?;
+
+    let weather_data = response
+        .json::<WeatherResponse>()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(weather_data)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![fetch_weather])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
