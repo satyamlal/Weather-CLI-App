f72b2324a6ba980626e32e15efce2cc347079c9b
Test <ssatyamlal@gmail.com>
2026-05-15 01:39:48 +0530

    fix the UI design

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,134 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/core";  
 import "./App.css";
+import {
+  Cloud,
+  CloudRain,
+  Sun,
+  Wind,
+  Droplets,
+  Thermometer,
+} from "lucide-react";
+
+type WeatherData = {
+  current: {
+    temperature_2m: number,
+    relative_humidity_2m: number,
+    apparent_temperature: number,
+    is_day: number,
+    percipitation: number,
+    weather_code: number,
+    wind_speed_10m: number,
+  },
+  hourly: {
+    time: string[],
+    temperature: number[],
+  },
+  daily: {
+    time: string[],
+    temperature_2m_max: number[],
+    temperature_2m_min: number[],
+  }
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
+  async function getWeather() {
+    try {
+      const response = await invoke<WeatherData>("fetch_weather");
+      setWeather(response);
+    } catch (error) {
+      console.error("Weather fetch failed!", error);
+    } finally {
+      setLoading(false);
+    }
   }
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+  useEffect(() => {
+    getWeather();
+  },[]);
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  if (loading) {
+    return (<div className="loading-screen">
+      <h1>Fetching Weather...</h1>
+    </div>)
+  }
+
+  if(!weather) {
+    return (
+      <div className="loading-screen">
+        <h1>Failed to load weather data!</h1>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    )
+  }
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+  const current = weather.current;
+
+  return (
+    <main className="app">
+      <section className="weather-card">
+        <div className="top-section">
+          <div>
+            <h1 className="temperature">
+              {current.temperature_2m}°C
+            </h1>
+
+            <p className="feels-like">
+              Feels like {current.apparent_temperature}°C
+            </p>
+          </div>
+          <div className="weather-icon">
+            {current.weather_code < 3 ? (
+              <Sun size={72} />
+            ) : current.weather_code < 60 ? (
+              <Cloud size={72} />
+            ) : (
+              <CloudRain size={72} />
+            )}
+          </div>
+        </div>
+
+        <div className="weather-stats">
+          <div className="stat-box">
+            <Wind/>
+            <span>{current.wind_speed_10m}km/h</span>
+          </div>
+
+          <div className="stat-box">
+            <Droplets/>
+            <span>{current.relative_humidity_2m}%</span>
+          </div>
+
+          <div className="stat-box">
+            <Thermometer/>
+            <span>{current.percipitation}mm</span>
+          </div>
+
+          <div className="stat-box">
+            <Thermometer/>
+            <span>{current.percipitation}mm</span>
+          </div>
+        </div>
+
+        <section className="forecast-section">
+          <h2>7 day Forecast</h2>
+
+          <div className="forecast-grid">
+            {weather.daily.time.map((day, index) => (
+              <div className="forecast-card" key={day}>
+                <p>{day}</p>
+
+                <h3>
+                  {weather.daily.temperature_2m_max[index]}° /
+                  {weather.daily.temperature_2m_min[index]}°
+                </h3>
+              </div>
+            ))}
+          </div>
+        </section>
+      </section>
     </main>
   );
 }
